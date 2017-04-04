8dcc6b25bfdd54fbdbb5f340bf135e042921389e
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-04-04 10:49:21 -0400

    Added Mitchel to example DB data

--- database/data.sql
+++ database/data.sql
@@ -3,9 +3,4 @@
 -- *****************************************************************************
 
 INSERT INTO parent
-VALUES  ('Anita','Torrence','anita@email.com','password','salt'),
-		('Mitchel','Mayle','mitchel@email.com','password','salt');
-
-INSERT INTO child
-VALUES  ('1','LittleBridge','Bridget','1000','10','password','salt'),
-		('2','LittlePete','Pete','1500','15','password','salt');
+VALUES ('Mitchel','Mayle','mitchel.mayle@gmail.com','U2o2PbxixctjzbHCnypRXfG5uLI=','K4yaqIN9p4w=');
