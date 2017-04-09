4aab8fe00d052cd83b92243b126c95c2be184fa5
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-04-09 12:53:54 -0400

    Created API to pull child's time from database

--- Capstone.Web/Scripts/time_tracking.js
+++ Capstone.Web/Scripts/time_tracking.js
@@ -1,11 +1,31 @@
 ﻿$(document).ready(function () {
 
-    // sends update request to database every 3 seconds
+    // sends update request to database every second
     setInterval(updateTime, 1000);
 
-    var seconds = 999;
+    // pulls username from hidden field in header
     username = $("#child_username").text();
 
+    // gets current time stored in child table
+    getTime();
+    var seconds = 0;
+
+    function getTime() {
+        $.ajax({
+            url: "api/getTime",
+            type: "GET",
+            data: {
+                username: username
+            },
+            dataType: "json"
+        }).done(function (data) {
+            console.log(data);
+            seconds = parseInt(data);
+        });
+        
+    }
+
+    // posts new remaining time
     function updateTime() {
         $.ajax({
             url: "api/updateTime",
